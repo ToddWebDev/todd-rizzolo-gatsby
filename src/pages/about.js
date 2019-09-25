272d6a3b0e422cfbeb6d45186cdbb0e3139ad97e
ToddWebDev <webdevtodd@gmail.com>
2019-09-24 19:22:03 -0700

    So much style

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,35 @@
 import React from "react"
+import { Link } from "gatsby"
+import Container from "../components/container"
 import Header from "../components/header"
+import styles from "./about.module.css"
+console.log(styles)
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Container>
+    <Link to="/">Home</Link>
     <Header headerText="About Gatsby" />
     <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
-  </div>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
 )
