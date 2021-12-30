7f0ac7e0d9457d4367d7a10d7a39032f7aff9b71
Todd Rizzolo <todd.rizzolo@lifeway.com>
2021-12-29 21:30:57 -0700

    Add gatsby-plugin-dark-mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    "gatsby-plugin-dark-mode",
   ],
   siteMetadata: {
     siteUrl: "https://www.toddrizzolo.com",
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,46 +2,64 @@ import React from "react"
 import Layout from "../components/layout"
 import PortfolioSection from "../components/portfolioSection"
 import useVH from "react-viewport-height"
+import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
 export default () => {
   useVH()
   return (
-    <Layout id="top">
-      <section>
-        <h2>Front-End Web Developer</h2>
-        <p>Toolbox</p>
-        <ul className="flex-list">
-          <li>Javascript</li>
-          <li>React</li>
-          <li>AngularJS</li>
-          <li>Webpack</li>
-          <li className="list-fill"></li>
-        </ul>
-        <ul>
-          <li>Gatsby</li>
-          <li>GraphQL</li>
-          <li>Accessibility</li>
-          <li className="list-fill"></li>
-        </ul>
-        <ul>
-          <li>User Interface Design</li>
-          <li>UX</li>
-          <li>Wireframes</li>
-          <li>Prototyping</li>
-          <li className="list-fill"></li>
-        </ul>
-      </section>
-      <PortfolioSection />
-      <section>
-        <h2>My Social Media</h2>
-        <p>Connect</p>
-        <p className="text-color-deemphasized">looking for connections...</p>
-      </section>
-      <section>
-        <a href="#top">
-          <i aria-hidden className="far fa-hand-point-up"></i> Back to top
-        </a>
-      </section>
-    </Layout>
+    <ThemeToggler>
+      {({ theme, toggleTheme }) => (
+        <Layout id="top">
+          <section>
+            <div style={{ position: "absolute", top: "3rem", right: "3rem" }}>
+              {theme === "light" ? (
+                <button onClick={() => toggleTheme("dark")}>
+                  <i aria-hidden className="fas fa-lightbulb"></i> Light Mode
+                </button>
+              ) : (
+                <button onClick={() => toggleTheme("light")}>
+                  <i aria-hidden className="fas fa-moon"></i> Dark Mode
+                </button>
+              )}
+            </div>
+            <h2>Front-End Web Developer</h2>
+            <p>Toolbox</p>
+            <ul className="flex-list">
+              <li>Javascript</li>
+              <li>React</li>
+              <li>AngularJS</li>
+              <li>Webpack</li>
+              <li className="list-fill"></li>
+            </ul>
+            <ul>
+              <li>Gatsby</li>
+              <li>GraphQL</li>
+              <li>Accessibility</li>
+              <li className="list-fill"></li>
+            </ul>
+            <ul>
+              <li>User Interface Design</li>
+              <li>UX</li>
+              <li>Wireframes</li>
+              <li>Prototyping</li>
+              <li className="list-fill"></li>
+            </ul>
+          </section>
+          <PortfolioSection />
+          <section>
+            <h2>My Social Media</h2>
+            <p>Connect</p>
+            <p className="text-color-deemphasized">
+              looking for connections...
+            </p>
+          </section>
+          <section style={{ display: "flex", justifyContent: "space-between" }}>
+            <a href="#top">
+              <i aria-hidden className="far fa-hand-point-up"></i> Back to top
+            </a>
+          </section>
+        </Layout>
+      )}
+    </ThemeToggler>
   )
 }
