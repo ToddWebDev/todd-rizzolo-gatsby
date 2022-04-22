5b924166117e20fcc43e4f49bb1ac5d8f7756738
Todd Rizzolo <todd.rizzolo@lifeway.com>
2022-04-22 08:46:45 -0600

    Add images to contact for google indexing

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,83 @@
 import React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { image } from "../styles/layout.module.css"
 
 export default () => (
   <Layout>
     <h1>Contact</h1>
-    <p>Send us a message!</p>
+    <p>Hey google, index these images...</p>
+    <StaticImage
+          className={image}
+          src="../images/profile-image.jpg"
+          alt="Todd Rizzolo on saddle of Mt Timpanogos"
+          placeholder="blurred"
+          width={250}
+          height={250}
+        />
+    <StaticImage
+      className={image}
+      src="../images/2016.png"
+      alt="Todd Rizzolo in Nashville"
+      placeholder="blurred"
+      width={250}
+      height={250}
+    />
+    <StaticImage
+      className={image}
+      src="../images/BryceCanyon_NavajoLoop.png"
+      alt="Todd Rizzolo in Bryce Canyon"
+      placeholder="blurred"
+      width={550}
+      height={350}
+    />
+    <StaticImage
+      className={image}
+      src="../images/cirque-traverse-2020.jpg"
+      alt="Todd Rizzolo at Snowbird"
+      placeholder="blurred"
+      width={400}
+      height={250}
+    />
+    <StaticImage
+      className={image}
+      src="../images/conrad-rizzolo-80th-bday.jpg"
+      alt="Conrad Rizzolo and Todd Rizzolo"
+      placeholder="blurred"
+      width={250}
+      height={250}
+    />
+    <StaticImage
+      className={image}
+      src="../images/head-shot-2018.png"
+      alt="Todd Rizzolo"
+      placeholder="blurred"
+      width={250}
+      height={250}
+    />
+    <StaticImage
+      className={image}
+      src="../images/internet-connection-meme.png"
+      alt="Todd Rizzolo Internet Meme"
+      placeholder="blurred"
+      width={450}
+      height={250}
+    />
+    <StaticImage
+      className={image}
+      src="../images/SugarloafPeak2020.jpg"
+      alt="Todd Rizzolo on Sugarloaf Peak"
+      placeholder="blurred"
+      width={350}
+      height={350}
+    />
+    <StaticImage
+      className={image}
+      src="../images/zion-canyon-2019.jpg"
+      alt="Todd Rizzolo on Angels Landing"
+      placeholder="blurred"
+      width={350}
+      height={450}
+    />
   </Layout>
 )
